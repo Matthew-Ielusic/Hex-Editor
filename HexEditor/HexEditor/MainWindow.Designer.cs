21be452079e26a5ed02f06b566bb351390a9eb5f
Matthew-Ielusic <matthew38@optonline.net>
2020-12-19 02:19:50 -0500

    Implement file saving

--- HexEditor/HexEditor/MainWindow.Designer.cs
+++ HexEditor/HexEditor/MainWindow.Designer.cs
@@ -28,69 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
             this.dataPanel = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // openButton
             // 
-            this.button1.Location = new System.Drawing.Point(51, 161);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open File";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.openButton.Location = new System.Drawing.Point(51, 161);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(83, 46);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
             // 
-            // button2
+            // resetButton
             // 
-            this.button2.Location = new System.Drawing.Point(51, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 39);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Reset";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.resetButton.Location = new System.Drawing.Point(51, 41);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(83, 39);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
             // dataPanel
             // 
             this.dataPanel.AutoScroll = true;
+            this.dataPanel.AutoScrollMinSize = new System.Drawing.Size(0, 416);
             this.dataPanel.BackColor = System.Drawing.SystemColors.ControlDark;
             this.dataPanel.Location = new System.Drawing.Point(230, 23);
             this.dataPanel.Name = "dataPanel";
-            this.dataPanel.Size = new System.Drawing.Size(554, 415);
+            this.dataPanel.Size = new System.Drawing.Size(578, 415);
             this.dataPanel.TabIndex = 1;
             // 
-            // button3
+            // saveButton
             // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(51, 223);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 46);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "Save File";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 272);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "(Not Implemented)";
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(51, 223);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(83, 46);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save File";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.openButton);
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.saveButton);
             this.groupBox1.Location = new System.Drawing.Point(14, 23);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(187, 309);
@@ -102,25 +93,23 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(810, 450);
+            this.ClientSize = new System.Drawing.Size(829, 450);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainWindow";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button resetButton;
         private System.Windows.Forms.Panel dataPanel;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.GroupBox groupBox1;
     }
 }
